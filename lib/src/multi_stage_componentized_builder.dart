60580cbc85169353b07163d506a5660eacd21df7
Luke Pighetti <lukepighetti@gmail.com>
2019-10-08 12:46:18 -0400

    fix methods

--- lib/src/multi_stage_componentized_builder.dart
+++ lib/src/multi_stage_componentized_builder.dart
@@ -1,6 +1,23 @@
+/// Based on https://github.com/uber/RIBs/blob/master/ios/RIBs/Classes/MultiStageComponentizedBuilder.swift
+
 import 'builder.dart';
 
+/// The base class of a builder that involves multiple stages of building
+/// a RIB. Witin the same pass, accesses to the component property shares
+/// the same instance. Once `finalStageBuild` is invoked, a new instance
+/// is returned from the component property, representing a new pass of
+/// the multi-stage building process.
+///
+/// - SeeAlso: SimpleMultiStageComponentizedBuilder
 class MultiStageComponentizedBuilder<Component, Router, DynamicBuildDependency> extends Buildable {
+  // Builder should not directly retain an instance of the component.
+  // That would make the component's lifecycle longer than the built
+  // RIB. Instead, whenever a new instance of the RIB is built, a new
+  // instance of the DI component should also be instantiated.
+  /// The DI component used for the current iteration of the multi-
+  /// stage build process. Once `finalStageBuild` method is invoked,
+  /// this property returns a separate new instance representing a
+  /// new pass of the multi-stage building process.
   Component get componentForCurrentBuildPass {
     if (_currentPassComponentIsDirty == false) {
       return _currentPassComponent;
@@ -8,6 +25,8 @@ class MultiStageComponentizedBuilder<Component, Router, DynamicBuildDependency> 
       final currentPassComponent = componentBuilder();
       final newComponent = componentBuilder();
 
+      // Ensure each invocation of componentBuilder produces a new
+      // component instance.
       if (_lastComponent == newComponent) {
         assert(false, "$this componentBuilder should produce new instances of component when build is invoked.");
       }
@@ -21,10 +40,18 @@ class MultiStageComponentizedBuilder<Component, Router, DynamicBuildDependency> 
     }
   }
 
+  /// Initializer.
   MultiStageComponentizedBuilder(this.componentBuilder);
 
   bool _currentPassComponentIsDirty = true;
 
+  /// Build a new instance of the RIB with the given dynamic dependency
+  /// as the last stage of this mult-stage building process.
+  ///
+  /// - note: Subsequent access to the `component` property after this
+  /// method is returned will result in a separate new instance of the
+  /// component, representing a new pass of the multi-stage building
+  /// process.
   Router finalStageBuildWithDynamicDependency(DynamicBuildDependency dynamicDependency) {
     final router = finalStageBuildWithComponent(componentForCurrentBuildPass, dynamicDependency);
 
@@ -33,6 +60,12 @@ class MultiStageComponentizedBuilder<Component, Router, DynamicBuildDependency> 
     return router;
   }
 
+  /// Abstract method that must be overriden to implement the RIB building
+  /// logic using the given component and dynamic dependency, as the last
+  /// building stage.
+  ///
+  /// - note: This method should never be invoked directly. Instead
+  /// consumers of this builder should invoke `finalStageBuildWithDynamicDependency`.
   Router finalStageBuildWithComponent(Component component, DynamicBuildDependency dynamicDependency) {
     throw UnsupportedError("This method should be overridden by the subclass.");
   }
@@ -42,15 +75,43 @@ class MultiStageComponentizedBuilder<Component, Router, DynamicBuildDependency> 
   dynamic _lastComponent;
 }
 
+/// A convenient base multi-stage builder class that does not require any
+/// build dynamic dependencies.
+///
+/// - note: If the build method requires dynamic dependency, please
+/// refer to [MultiStageComponentizedBuilder].
+///
+/// - SeeAlso: [MultiStageComponentizedBuilder]
 class SimpleMultiStageComponentizedBuilder<Component, Router>
     extends MultiStageComponentizedBuilder<Component, Router, void> {
+  /// Initializer.
   SimpleMultiStageComponentizedBuilder(Component Function() componentBuilder) : super(componentBuilder);
 
+  /// This method should not be directly invoked.
   @override
   Router finalStageBuildWithComponent(Component component, void _) {
+    return finalStageBuildWithComponentSimple(component);
+  }
+
+  /// Abstract method that must be overriden to implement the RIB building
+  /// logic using the given component.
+  ///
+  /// - note: This method should never be invoked directly. Instead
+  /// consumers of this builder should invoke `finalStageBuild()`.
+  /// - parameter component: The corresponding DI component to use.
+  /// - returns: The router of the RIB.
+  Router finalStageBuildWithComponentSimple(Component component) {
     throw UnsupportedError("This method should be overridden by the subclass.");
   }
 
+  /// Build a new instance of the RIB as the last stage of this mult-
+  /// stage building process.
+  ///
+  /// - note: Subsequent access to the `component` property after this
+  /// method is returned will result in a separate new instance of the
+  /// component, representing a new pass of the multi-stage building
+  /// process.
+  /// - returns: The router of the RIB.
   Router finalStageBuild() {
     return finalStageBuildWithDynamicDependency(null);
   }
