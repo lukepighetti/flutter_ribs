d3cec137dc330f791a29298c461529d9b7ddbb3f
Luke Pighetti <lukepighetti@gmail.com>
2019-10-08 12:51:28 -0400

    implement Presenter, PresentableInteractor

--- lib/src/presentable_interactor.dart
+++ lib/src/presentable_interactor.dart
@@ -1,3 +1,18 @@
 /// Base on https://github.com/uber/RIBs/blob/master/ios/RIBs/Classes/PresentableInteractor.swift
 
-/// TODO: implement PresentableInteractor
+import 'interactor.dart';
+
+/// Base class of an `Interactor` that actually has an associated `Presenter` and `View`.
+class PresentableInteractor<PresenterType> extends Interactor {
+  /// The `Presenter` associated with this `Interactor`.
+  final PresenterType presenter;
+
+  /// Initializer.
+  ///
+  /// - note: This holds a strong reference to the given `Presenter`.
+  ///
+  /// - parameter presenter: The presenter associated with this `Interactor`.
+  PresentableInteractor(this.presenter);
+
+  /// Leak detection is not implemented
+}
--- lib/src/presenter.dart
+++ lib/src/presenter.dart
@@ -1,3 +1,17 @@
 /// Based on https://github.com/uber/RIBs/blob/master/ios/RIBs/Classes/Presenter.swift
 
-/// TODO: implement Presenter
+/// The base protocol for all `Presenter`s.
+abstract class Presentable {}
+
+/// The base class of all `Presenter`s. A `Presenter` translates business models into values the corresponding
+/// `ViewController` can consume and display. It also maps UI events to business logic method, invoked to
+/// its listener.
+class Presenter<ViewControllerType> extends Presentable {
+  /// The view controller of this presenter.
+  final ViewControllerType viewController;
+
+  /// Initializer.
+  ///
+  /// - parameter viewController: The `ViewController` of this `Pesenters`.
+  Presenter(this.viewController);
+}
