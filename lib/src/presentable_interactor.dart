bede15ff1ccaefb7e16b1e1a6a3bade4e77291d2
Luke Pighetti <lukepighetti@gmail.com>
2019-10-08 12:42:33 -0400

    add todo for Presenter and PresentableInteractor

---
+++ lib/src/presentable_interactor.dart
@@ -0,0 +1,3 @@
+/// Base on https://github.com/uber/RIBs/blob/master/ios/RIBs/Classes/PresentableInteractor.swift
+
+/// TODO: implement PresentableInteractor
---
+++ lib/src/presenter.dart
@@ -0,0 +1,3 @@
+/// Based on https://github.com/uber/RIBs/blob/master/ios/RIBs/Classes/Presenter.swift
+
+/// TODO: implement Presenter
