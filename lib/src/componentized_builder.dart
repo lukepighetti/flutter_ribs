5ec5e3f6ce1cbce41c528a0126788a7809176b79
Luke Pighetti <lukepighetti@gmail.com>
2019-10-08 10:19:41 -0400

    add ComponentizedBuilder tests

--- lib/src/componentized_builder.dart
+++ lib/src/componentized_builder.dart
@@ -20,10 +20,7 @@ import 'router.dart';
 /// implement the actual RIB building logic, with the given component
 /// and dynamic dependency.
 /// - SeeAlso: SimpleComponentizedBuilder
-// open class ComponentizedBuilder<Component, Router, DynamicBuildDependency, DynamicComponentDependency>: Buildable {
-
-class ComponentizedBuilder<DynamicBuildDependency, DynamicComponentDependency>
-    extends Buildable {
+class ComponentizedBuilder<Component, Router, DynamicBuildDependency, DynamicComponentDependency> extends Buildable {
   // Builder should not directly retain an instance of the component.
   // That would make the component's lifecycle longer than the built
   // RIB. Instead, whenever a new instance of the RIB is built, a new
@@ -32,9 +29,6 @@ class ComponentizedBuilder<DynamicBuildDependency, DynamicComponentDependency>
   ///
   /// - parameter componentBuilder: The closure to instantiate a new
   /// instance of the DI component that should be paired with this RIB.
-  // public init(componentBuilder: @escaping (DynamicComponentDependency) -> Component) {
-  //     self.componentBuilder = componentBuilder
-  // }
 
   ComponentizedBuilder({
     @required Component Function(DynamicComponentDependency) componentBuilder,
@@ -47,14 +41,10 @@ class ComponentizedBuilder<DynamicBuildDependency, DynamicComponentDependency>
   /// - parameter dynamicComponentDependency: The dynamic dependency to
   /// use to instantiate the component.
   /// - returns: The router of the RIB.
-  // public final func build(withDynamicBuildDependency dynamicBuildDependency: DynamicBuildDependency, dynamicComponentDependency: DynamicComponentDependency) -> Router {
-  //     return build(withDynamicBuildDependency: dynamicBuildDependency, dynamicComponentDependency: dynamicComponentDependency).1
-  // }
 
-  Router buildRouter(DynamicBuildDependency dynamicBuildDependency,
-      DynamicComponentDependency dynamicComponentDependency) {
-    return buildTuple(dynamicBuildDependency, dynamicComponentDependency)
-        .router;
+  Router buildRouter(
+      DynamicBuildDependency dynamicBuildDependency, DynamicComponentDependency dynamicComponentDependency) {
+    return buildComponentAndRouter(dynamicBuildDependency, dynamicComponentDependency).router;
   }
 
   /// Build a new instance of the RIB with the given dynamic dependencies.
@@ -64,46 +54,19 @@ class ComponentizedBuilder<DynamicBuildDependency, DynamicComponentDependency>
   /// - parameter dynamicComponentDependency: The dynamic dependency to
   /// use to instantiate the component.
   /// - returns: The tuple of component and router of the RIB.
-  // public final func build(withDynamicBuildDependency dynamicBuildDependency: DynamicBuildDependency, dynamicComponentDependency: DynamicComponentDependency) -> (Component, Router) {
-  //     let component = componentBuilder(dynamicComponentDependency)
-
-  //     // Ensure each componentBuilder invocation produces a new component
-  //     // instance.
-  //     let newComponent = component as AnyObject
-  //     if lastComponent === newComponent {
-  //         assertionFailure("\(self) componentBuilder should produce new instances of component when build is invoked.")
-  //     }
-  //     lastComponent = newComponent
-
-  //     return (component, build(with: component, dynamicBuildDependency))
-  // }
-  _BuildTuple buildTuple(DynamicBuildDependency dynamicBuildDependency,
-      DynamicComponentDependency dynamicComponentDependency) {
+  _BuildTuple<Component, Router> buildComponentAndRouter(
+      DynamicBuildDependency dynamicBuildDependency, DynamicComponentDependency dynamicComponentDependency) {
     final component = _componentBuilder(dynamicComponentDependency);
+    final newComponent = _componentBuilder(dynamicComponentDependency);
 
-    /// TODO: create a copy of a component without being a linked instance
-    final newComponent = component as dynamic;
-
-    assert(component != newComponent,
-        "$this componentBuilder should produce new instances of component when build is invoked.");
+    if (_lastComponent == newComponent) {
+      assert(false, "$this componentBuilder should produce new instances of component when build is invoked.");
+    }
 
     _lastComponent = newComponent;
 
     return _BuildTuple(component, build(component, dynamicBuildDependency));
   }
-  //     public final func build(withDynamicBuildDependency dynamicBuildDependency: DynamicBuildDependency, dynamicComponentDependency: DynamicComponentDependency) -> (Component, Router) {
-  //     let component = componentBuilder(dynamicComponentDependency)
-
-  //     // Ensure each componentBuilder invocation produces a new component
-  //     // instance.
-  //     let newComponent = component as AnyObject
-  //     if lastComponent === newComponent {
-  //         assertionFailure("\(self) componentBuilder should produce new instances of component when build is invoked.")
-  //     }
-  //     lastComponent = newComponent
-
-  //     return (component, build(with: component, dynamicBuildDependency))
-  // }
 
   /// Abstract method that must be overriden to implement the RIB building
   /// logic using the given component and dynamic dependency.
@@ -113,24 +76,17 @@ class ComponentizedBuilder<DynamicBuildDependency, DynamicComponentDependency>
   /// - parameter component: The corresponding DI component to use.
   /// - parameter dynamicBuildDependency: The given dynamic dependency.
   /// - returns: The router of the RIB.
-  // open func build(with component: Component, _ dynamicBuildDependency: DynamicBuildDependency) -> Router {
-  //     fatalError("This method should be oevrriden by the subclass.")
-  // }
 
-  Router build(
-      Component component, DynamicBuildDependency dynamicBuildDependency) {
-    throw UnimplementedError(
-        "This method should be oevrriden by the subclass.");
+  Router build(Component component, DynamicBuildDependency dynamicBuildDependency) {
+    throw UnimplementedError("This method should be oevrriden by the subclass.");
   }
 
   // MARK: - Private
-  // private let componentBuilder: (DynamicComponentDependency) -> Component
   final Component Function(DynamicComponentDependency) _componentBuilder;
-  // private weak var lastComponent: AnyObject?
   dynamic _lastComponent;
 }
 
-class _BuildTuple {
+class _BuildTuple<Component, Router> {
   _BuildTuple(this.component, this.router);
 
   final Component component;
@@ -145,59 +101,19 @@ class _BuildTuple {
 /// requires dynamic dependency, please refer to `DynamicComponentizedBuilder`.
 /// If both require dynamic dependencies, please use `ComponentizedBuilder`.
 /// - SeeAlso: ComponentizedBuilder
-// open class SimpleComponentizedBuilder<Component, Router>: ComponentizedBuilder<Component, Router, (), ()> {
-class SimpleComponentizedBuilder<DynamicBuildDependency,
-        DynamicComponentDependency>
-    extends ComponentizedBuilder<DynamicBuildDependency,
-        DynamicComponentDependency> {
-  /// Initializer.
-  ///
-  /// - parameter componentBuilder: The closure to instantiate a new
-  /// instance of the DI component that should be paired with this RIB.
-  // #if compiler(>=5.0)
-  //     public init(componentBuilder: @escaping () -> Component) {
-  //         super.init(componentBuilder: componentBuilder)
-  //     }
-  // #else
-  //     public override init(componentBuilder: @escaping () -> Component) {
-  //         super.init(componentBuilder: componentBuilder)
-  //     }
-  // #endif
+class SimpleComponentizedBuilder<Component, Router> extends ComponentizedBuilder<Component, Router, void, void> {
   SimpleComponentizedBuilder(Function(Component) componentBuilder);
 
-  /// This method should not be directly invoked.
-  // public final override func build(with component: Component, _ dynamicDependency: ()) -> Router {
-  //     return build(with: component)
-  // }
   @override
-  Router build(
-      Component component, DynamicBuildDependency dynamicBuildDependency) {
+  Router build(Component component, dynamic _) {
     return buildSimple(component);
   }
 
-  /// Abstract method that must be overriden to implement the RIB building
-  /// logic using the given component.
-  ///
-  /// - note: This method should never be invoked directly. Instead
-  /// consumers of this builder should invoke `build(with dynamicDependency:)`.
-  /// - parameter component: The corresponding DI component to use.
-  /// - returns: The router of the RIB.
-  // open func build(with component: Component) -> Router {
-  //     fatalError("This method should be oevrriden by the subclass.")
-  // }
   Router buildSimple(Component component) {
-    throw UnimplementedError(
-        "This method should be oevrriden by the subclass.");
+    throw UnimplementedError("This method should be oevrriden by the subclass.");
   }
 
-  /// Build a new instance of the RIB.
-  ///
-  /// - returns: The router of the RIB.
-  // public final func build() -> Router {
-  //     return build(withDynamicBuildDependency: (), dynamicComponentDependency: ())
-  // }
-
-  Router buildSimpler() {
-    return buildTuple(null, null).router;
+  Router buildInstance() {
+    return buildComponentAndRouter(null, null).router;
   }
 }
---
+++ test/componentized_builder_test.dart
@@ -0,0 +1,44 @@
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_test/flutter_test.dart';
+import 'package:ribs/ribs.dart';
+import 'package:test/test.dart';
+
+// import 'package:test/test.dart';
+
+main() {
+  group("ComponentizedBuilder", () {
+    test("builder returns same instance, verify assertion", () async {
+      final component = MockComponent();
+      final sameInstanceBuilder = MockComponentizedBuilder((_) => component);
+      sameInstanceBuilder.buildHandler = (component, _) => MockSimpleRouter();
+
+      expect(() => sameInstanceBuilder.buildRouter(null, null), returnsNormally);
+      expect(() => sameInstanceBuilder.buildRouter(null, null), throwsA(TypeMatcher<AssertionError>()));
+    });
+
+    test("builder returns same instance, verify assertion", () async {
+      final sameInstanceBuilder = MockComponentizedBuilder((_) => MockComponent());
+      sameInstanceBuilder.buildHandler = (component, _) => MockSimpleRouter();
+
+      expect(() => sameInstanceBuilder.buildRouter(null, null), returnsNormally);
+      expect(() => sameInstanceBuilder.buildRouter(null, null), returnsNormally);
+    });
+  });
+}
+
+class MockComponent {}
+
+class MockSimpleRouter {}
+
+class MockComponentizedBuilder extends ComponentizedBuilder<MockComponent, MockSimpleRouter, void, void> {
+  MockComponentizedBuilder(MockComponent Function(void) componentBuilder) : super(componentBuilder: componentBuilder);
+
+  MockSimpleRouter Function(MockComponent, void) buildHandler;
+
+  @override
+  MockSimpleRouter build(MockComponent component, dynamic dynamicBuildDependency) {
+    if (buildHandler == null) throw StateError("buildHandler must not be null");
+
+    return buildHandler(component, dynamicBuildDependency);
+  }
+}
