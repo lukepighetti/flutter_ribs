1b952673e34ef11c4b43bf020b9fbb8f9b0ef934
Luke Pighetti <lukepighetti@gmail.com>
2019-10-09 13:41:21 -0400

    add Navigator present method

--- lib/src/window.dart
+++ lib/src/window.dart
@@ -1,8 +1,15 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'view_controllable.dart';
 
 /// The controller used for launching [ViewControllable]
 class WindowController {
+  static final navigator = GlobalKey<NavigatorState>();
+
+  /// Pushes this view to the top of the [Navigator] stack
+  static present(WidgetBuilder builder) async {
+    navigator.currentState.push(MaterialPageRoute(builder: builder));
+  }
+
   final _currentView = ValueNotifier<ViewControllable>(null);
 
   ValueNotifier<Widget> get currentView => _currentView;
